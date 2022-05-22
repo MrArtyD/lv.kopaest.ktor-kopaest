70853846bfafc720982907fd53db09e884273264
MrArtyD <artjom.denisovs@gmail.com>
2022-05-22 20:17:48 +0300

    add post request to cors

--- src/main/kotlin/lv/kopaest/plugins/CORS.kt
+++ src/main/kotlin/lv/kopaest/plugins/CORS.kt
@@ -8,6 +8,7 @@ fun Application.configureCors() {
     install(CORS) {
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
         allowHeader(HttpHeaders.Authorization)
